226691a12f1df5e89192369c5e6dd136bf6666f2
lauricdd <lauri_cdd@hotmail.com>
2019-11-13 09:01:57 +0100

    code refactoring

--- basics/tensors_operations.py
+++ basics/tensors_operations.py
@@ -4,7 +4,7 @@
 import numpy as np
 import tensorflow as tf
 
-# Operations on tensors =? tensorflow 2.0
+# Operations on tensors - Tensorflow 2.0
 # ---------------
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
